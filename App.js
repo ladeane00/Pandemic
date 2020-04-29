604f8ee9fe89a093e9e37f5ba6f4e3591934b45c
EliJaghab <elijaghab@gmail.com>
2020-04-28 20:00:14 -0400

    Functional Search

--- App.js
+++ App.js
@@ -1,74 +1,67 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, Alert, FlatList, Button, Text, View, StyleSheet,TextInput } from 'react-native';
-import Swiper from 'react-native-swiper'; // 1.5.13
 import PureChart from 'react-native-pure-chart';
 
 export default class App extends Component {
 
   // Create States to Store Variables
   constructor(props) {
-
     super(props);
-
-    this.state = {
-      timeChartTest: [],
-      custLoc: '',
-      custLocTemp: '',
-      latitude: '',
-      longitude: '',
-      latitudeTemp: '',
-      longitudeTemp: '',
-      text: '',
-      data: [],
-      country: 'Loading...',
-    };
-    global.custLoc = "";
-    global.lat = "";
-    global.lon = "";
-    this.arrayholder = [];
+      this.state = {
+        timeChart: [],
+        latitude: '',
+        longitude: '',
+        text: '',
+        listData: [],
+        loc: 0,
+        locArray: [],
+        allData: null,
+     };
+        global.lat = "";
+        global.lon = "";
+        this.arrayholder = [];
   }
 
   componentDidMount() {
-    //this.getTestData();
-    this.getCaseCount();
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-          });
-        },
-
-        (error) => this.setState({ error: error.message, latitude:40.0379, longitude:75.3433 }),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
-      );
-  }
-
-  getTestData() {
-    return fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        // console.log(responseJson);
-        this.setState({
-          isLoading: false,
-          data: responseJson,
-        }, () => {
-          // In this block you can do something with new state.
-          this.arrayholder = responseJson;
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.getData();
+    this.getLocation();
   }
 
   GetFlatListItem(item) {
+    let number = 0;
+    for (var i = 0; i < this.state.locArray.length; i++) {
+      if (this.state.locArray[i] == item) {
+        number = i;
+
+      }
+    }
+
+    this.setChart(number);
+    
     this.setState({
-      country: this.state.item
+      loc: item,
     })
   }
 
+  setChart(num) {
+    let timeData = [];
+
+      let dateSize = Object.entries(Object.fromEntries(
+        Object.entries(Object.values(this.state.allData)[num]).map(([key, value]) => [key, value])
+        ).dates).length;
+
+      for (var i = 0; i < dateSize; i++) {
+        timeData[i] = {x: Object.entries(Object.fromEntries(
+          Object.entries(Object.values(this.state.allData)[num]).map(([key, value]) => [key, value])
+        ).dates)[i][0], y: Object.entries(Object.fromEntries(
+          Object.entries(Object.values(this.state.allData)[num]).map(([key, value]) => [key, value])
+        ).dates)[i][1].cases};
+      }
+      this.setState({timeChart: timeData});
+  }
+
+
+  // FlatList Functions
   searchData(text) {
     const newData = this.arrayholder.filter(item => {
       const itemData = item.toUpperCase();
@@ -77,7 +70,7 @@ export default class App extends Component {
     });
  
     this.setState({
-      data: newData,
+      listData: newData,
       text: text
       })
     }
@@ -108,11 +101,22 @@ export default class App extends Component {
     Object.entries(Object.values(data)[0]).map(([key, value]) => [key, value])
   ).dates)*/
 
-//CoronaVirus API Goes here
-  getLocation(){
+//User Location 
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+          });
+        },
+        (error) => this.setState({ error: error.message, latitude:40.0379, longitude:75.3433 }),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+      );
     this.setState({
-      latitudeTemp:this.state.latitude,
-      longitudeTemp:this.state.longitude,
+      latitude: parseInt(this.state.latitude),
+      longitude: parseInt(this.state.longitude),
     })
   }
 
@@ -120,37 +124,53 @@ export default class App extends Component {
     global.custLoc = this.state.custLocTemp;
 }
 
-getCaseCount() {
-  global.lat = parseInt(this.state.latitude);
-  global.lon = parseInt(this.state.longitude);
+getData() {
   fetch(`https://coronadatascraper.com/timeseries-byLocation.json`)
   .then(response => response.json())
   .then((data) => {
-    //Countries Keys Array
-    this.setState({data: Object.keys(data)}, () => {
+    //sets a data variable with everything
+    this.setState({allData: data});
+
+    //Location Keys Array
+    this.setState({locArray: Object.keys(data)});
+
+    let locSize = Object.keys(data).length;
+
+    let coordData = [];
+    for (var i = 0; i < locSize; i++) {
+      coordData[i] = Object.fromEntries(
+        Object.entries(Object.values(data)[i]).map(([key, value]) => [key, value])
+      ).coordinates;
+    }
+
+    //List setter
+    this.setState({listData: Object.keys(data)}, () => {
       this.arrayholder = Object.keys(data);
     });
     let timeData = [];
     // Number of Countries
-    let size = Object.entries(Object.fromEntries(
-      Object.entries(Object.values(data)[12]).map(([key, value]) => [key, value])
-    ).dates).length;
     //
-    for (var i = 0; i < size; i++) {
-      timeData[i] = {x: Object.entries(Object.fromEntries(
-        Object.entries(Object.values(data)[12]).map(([key, value]) => [key, value])
-      ).dates)[i][0], y: Object.entries(Object.fromEntries(
-        Object.entries(Object.values(data)[12]).map(([key, value]) => [key, value])
-      ).dates)[i][1].cases};
-    }
+      let dateSize = Object.entries(Object.fromEntries(
+        Object.entries(Object.values(data)[0]).map(([key, value]) => [key, value])
+        ).dates).length;
+      for (var i = 0; i < dateSize; i++) {
+        timeData[i] = {x: Object.entries(Object.fromEntries(
+          Object.entries(Object.values(data)[0]).map(([key, value]) => [key, value])
+        ).dates)[i][0], y: Object.entries(Object.fromEntries(
+          Object.entries(Object.values(data)[0]).map(([key, value]) => [key, value])
+        ).dates)[i][1].cases};
+      }
     
-    this.setState({timeChartTest: timeData})
+    this.setState({coorArray: coordData});
+
+    this.setState({timeChart: timeData})
   });
+
 }
 
   render() {
-    const { timeChartTest } = this.state;
-    //console.log(timeChartTest);
+    const { timeChart } = this.state;
+    //console.log(timeChart);
 
     /*if (this.state.isLoading) {
       return (
@@ -174,7 +194,7 @@ getCaseCount() {
             returnKeyType="go" />
  
             <FlatList
-            data={this.state.data}
+            data={this.state.listData}
             keyExtractor={ (index) => index }
             ItemSeparatorComponent={this.itemSeparator}
             renderItem={({ item }) => <Text style={styles.row}
@@ -191,7 +211,7 @@ getCaseCount() {
             width={'40%'}
             height={300}
             gap={25}
-            data={timeChartTest}
+            data={timeChart}
             padding={0}
             type="line"
             numberOfYAxisGuideLine={10}
@@ -200,8 +220,12 @@ getCaseCount() {
             labelColor='black'
             primaryColor='red'/>
         </View>
-        <Text style={styles.otherText}> {this.state.country} </Text>
+        <Text style={styles.otherText}> {this.state.loc} </Text>
         <Text style={styles.otherText}>  </Text>
+        <Button
+            title = 'Use Current Location'
+            onPress={() => this.submitLocation()}
+        />
 
 
         </View>
